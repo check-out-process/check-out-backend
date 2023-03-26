2438c597cabb5a16ee93c4ccee97f0db81e146b9
עומר עדני <omer90050@gmail.com>
2023-03-27 00:33:58 +0300

    add typeorm init

---
+++ src/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: 'localhost',
+        port: 1433,
+        username: 'sa',
+        password: 'Password1',
+        database: 'AssaGay',
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
