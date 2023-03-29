aec81dfbf76f9788dfb7dbed26307924aac51be0
Amit Assa <amitassa9@gmail.com>
2023-03-29 17:36:02 +0300

    beds route added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DepartmentModule } from './department/department.module';
 import { RoomsModule } from './rooms/rooms.module';
+import { BedsModule } from './beds/beds.module';
 
 @Module({
-  imports: [DepartmentModule, RoomsModule],
+  imports: [DepartmentModule, RoomsModule, BedsModule],
 
 })
 export class AppModule {}
