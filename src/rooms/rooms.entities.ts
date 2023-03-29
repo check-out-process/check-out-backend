f42446b6e32cbf1d2460ceeaa2d7aaf9f7ce7937
Amit Assa <amitassa9@gmail.com>
2023-03-29 09:51:05 +0300

    room entity defined

--- src/rooms/rooms.entities.ts
+++ src/rooms/rooms.entities.ts
@@ -1,7 +1,17 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
 export class Room{
+    @PrimaryColumn({unique: true})
     Id: number;
+
+    @Column({unique: true})
     UUID: string;
+
+    @Column()
     departmentId: number;
+
+    @Column()
     roomName: string;
 
     constructor(id?: number, uuid?: string, departmentID?: number, name?: string){
