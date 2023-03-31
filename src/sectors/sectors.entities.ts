0acee1fa6c69ed633e65f1d566f8b064744fa05a
Amit Assa <amitassa9@gmail.com>
2023-03-31 19:39:14 +0300

    Sector responsible entity created

--- src/sectors/sectors.entities.ts
+++ src/sectors/sectors.entities.ts
@@ -1,3 +1,16 @@
+import { IsString, IsNumber } from "class-validator";
+
 export class Sector {
-    
+
+}
+
+export class SectorResponsible {
+    @IsNumber()
+    id: number;
+
+    @IsString()
+    sectorName: string;
+
+    @IsNumber()
+    defaultResponsibleUserId: number;
 }
