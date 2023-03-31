8b5aa13b47ac8c2096f87a1489b2e72f962874eb
Amit Assa <amitassa9@gmail.com>
2023-03-31 19:35:14 +0300

    sectors added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DepartmentModule } from './department/department.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { BedsModule } from './beds/beds.module';
+import { SectorsModule } from './sectors/sectors.module';
 
 @Module({
-  imports: [DepartmentModule, RoomsModule, BedsModule],
+  imports: [DepartmentModule, RoomsModule, BedsModule, SectorsModule],
 
 })
 export class AppModule {}
