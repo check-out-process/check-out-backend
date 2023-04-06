b1d5a3fd3201f51759002409a769987c6a644ba9
Amit Assa <amitassa9@gmail.com>
2023-04-06 12:55:00 +0300

    users module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DepartmentModule } from './department/department.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { BedsModule } from './beds/beds.module';
 import { SectorsModule } from './sectors/sectors.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DepartmentModule, RoomsModule, BedsModule, SectorsModule],
+  imports: [DepartmentModule, RoomsModule, BedsModule, SectorsModule, UsersModule],
 
 })
 export class AppModule {}
