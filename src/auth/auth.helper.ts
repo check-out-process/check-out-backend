a8cd61997efd1609bf89567b37639e5e6a138f08
afekl <afekev55@gmail.com>
2023-05-27 15:35:26 +0300

    add try catch to getUserDecoded

--- src/auth/auth.helper.ts
+++ src/auth/auth.helper.ts
@@ -1,5 +1,10 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { verify } from 'jsonwebtoken';
 
 export const getUserDecoded = (token: string) => {
-    return verify(token, process.env.ACCESS_TOKEN_SECRET);
+    try {
+        return verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (err) {
+        throw new HttpException("invalid token", HttpStatus.UNAUTHORIZED);
+      }
 }
