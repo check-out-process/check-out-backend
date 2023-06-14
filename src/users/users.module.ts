1d72f2e74b0575b10c241e67a6738e62e0f33df5
lior bahar <lior.bahar1412@gmail.com>
2023-06-14 18:29:20 +0300

    fix update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from 'src/database/database.module';
@@ -6,7 +6,6 @@ import { usersProviders } from './users.provides';
 import { JobsModule } from 'src/jobs/jobs.module';
 import { RolesModule } from 'src/roles/roles.module';
 import { SectorsModule } from 'src/sectors/sectors.module';
-import { TokenModule } from 'src/tokens/tokens.module';
 
 @Module({
   imports: [DatabaseModule, JobsModule, RolesModule, SectorsModule],
