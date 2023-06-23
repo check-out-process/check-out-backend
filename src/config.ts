3cc96877ac4c5fa5acfc3cb586681001c6e5026a
afekl <afekev55@gmail.com>
2023-06-23 13:55:59 +0300

    remove whatappacoount config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const Config = {
     sectorFinsihUrl: 'http://checkout.cs.colman.ac.il/scanBed',
-    whatsAppAccountSeed: 'ACc1554b991c424a9e18071b088bc01b29',
-    whatsAppAuthToken: 'cb9c4e1bfc52a3265dff012c2f435db7',
+    whatsAppAccountSeed: '',
+    whatsAppAuthToken: '',
     webUrl: 'http://checkout.cs.colman.ac.il'
 }
 
