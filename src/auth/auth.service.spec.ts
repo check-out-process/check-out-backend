83bd24fb70248eff72f39d4645c3fae7d2a9da4f
afekl <afekev55@gmail.com>
2023-07-28 11:48:27 +0300

    add register tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { TokensService } from '../tokens/tokens.service';
 import { UsersService } from '../users/users.service';
 import { createMock } from '@golevelup/ts-jest';
-import { LogInParams } from '@checkout/types';
+import { LogInParams, UserCreationParams } from '@checkout/types';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('Auth Service', () => {
@@ -23,24 +23,52 @@ describe('Auth Service', () => {
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+    describe('login', () => {
+        it('null phoneNumber should throw error', async () => {
+            const logInParams: LogInParams = {
+                phoneNumber: null,
+                password: 'string',
+                tokens: []
+            }
+            await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        });
 
-    it('login with null phoneNumber should throw error', async () => {
-        const logInParams: LogInParams = {
-            phoneNumber: null,
-            password: 'string',
-            tokens: []
-        }
-        await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        it('null password should throw error', async () => {
+            const logInParams: LogInParams = {
+                phoneNumber: 'string',
+                password: null,
+                tokens: []
+            }
+            await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        });
+    })
 
-    });
+    describe('register', () => {
+        it('null phoneNumber should throw error', async () => {
+            const logInParams: UserCreationParams = {
+                id: 1,
+                fullname: 'string',
+                username: 'string',
+                password: 'string',
+                jobId: 'string',
+                roleId: 'string',
+                phoneNumber: null
+            }
+            await expect(service.register(logInParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
+        });
 
-    it('login with null password should throw error', async () => {
-        const logInParams: LogInParams = {
-            phoneNumber: 'string',
-            password: null,
-            tokens: []
-        }
-        await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        it('null password should throw error', async () => {
+            const logInParams: UserCreationParams = {
+                id: 1,
+                fullname: 'string',
+                username: 'string',
+                password: null,
+                jobId: 'string',
+                roleId: 'string',
+                phoneNumber: 'string'
+            }
+            await expect(service.register(logInParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
+        });
+    })
 
-    });
 });
