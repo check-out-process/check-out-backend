2aa07be666a19755d9429ff4aee268d1537b7796
afekl <afekev55@gmail.com>
2023-07-29 12:48:55 +0300

    pretify code

--- src/sectors/sectors.service.ts
+++ src/sectors/sectors.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException, HttpStatus, Inject } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { Repository } from 'typeorm';
 import * as _ from 'lodash';
@@ -8,7 +8,6 @@ import { User } from '../users/users.entities';
 import { UsersHelper } from '../users/users.helper';
 import { ProcessTemplate, ProcessType } from '../process-templates/process-templates.entities';
 import { ProcessTemplatesHelper } from '../process-templates/process-templates.helper';
-import { log } from 'console';
 
 @Injectable()
 export class SectorsService {
@@ -25,11 +24,10 @@ export class SectorsService {
     public async getSector(sectorID: string): Promise<Sector> {
         try {
             const sector = await this.sectorsRepo.findOne({ where: { id: sectorID } })
-            log(sector.processTypes);
             return sector;
         }
-        catch {
-            //ToDo: throw err
+        catch (error) {
+            throw error;
         }
     }
 
@@ -44,17 +42,16 @@ export class SectorsService {
 
     public async addSector(data: SectorCreationParams): Promise<Sector> {
         let newSector: Sector = this.sectorsRepo.create()
+
         newSector.id = randomUUID();
         newSector.name = data.name;
         newSector = await this.updateDefaultUser(newSector, data.defaultResponsibleUserId, 'defaultResponsibleUser');
         newSector = await this.updateDefaultUser(newSector, data.defaultCommittingUsersId, 'defaultCommittingUser');
-
-
         newSector = await this.updateProcessTypes(newSector, data.processTypes);
         newSector = await this.updateOfficials(newSector, data.responsibleUsersIds, "responsible");
         newSector = await this.updateOfficials(newSector, data.committingUsersIds, "committer");
         newSector = await this.updateRelatedProcesses(newSector, data.relatedProcessIds);
-        
+
         return await this.sectorsRepo.save(newSector);
     }
 
@@ -74,32 +71,34 @@ export class SectorsService {
 
     public async addComittersToSector(sectorID: string, data: SectorPatchAddUsersParams): Promise<Sector> {
         let sectorToUpdate: Sector = await this.getSector(sectorID);
-        // change to promise.all if necessary
         data.userIds.forEach(async (id) => {
             const user: User = await UsersHelper.getUserById(id);
             (await sectorToUpdate.committingUsers).push(user);
         })
+
         return this.sectorsRepo.save(sectorToUpdate);
     }
 
     public async addResponsiblesToSector(sectorID: string, data: SectorPatchAddResponsiblesParams): Promise<Sector> {
         let sectorToUpdate: Sector = await this.getSector(sectorID);
-        // change to promise.all if necessary
+
         data.userIds.forEach(async (id) => {
             const user: User = await UsersHelper.getUserById(id);
             (await sectorToUpdate.responsibleUsers).push(user);
         })
+
         return this.sectorsRepo.save(sectorToUpdate);
     }
 
     public async deleteSector(sectorID: string): Promise<Sector> {
         let sectorToDelete: Sector = await this.getSector(sectorID);
+
         try {
             await this.sectorsRepo.remove(sectorToDelete);
             return sectorToDelete;
         }
         catch (error) {
-            log(error)
+            throw error;
         }
     }
 
@@ -107,8 +106,10 @@ export class SectorsService {
         if (!userId) {
             return sector;
         }
+
         const user: User = await UsersHelper.getUserById(userId);
         sector[key] = user;
+
         return sector;
     }
 
@@ -116,31 +117,24 @@ export class SectorsService {
         if (!processType_ids || _.isEmpty(processType_ids)) {
             return sector
         }
+
         let processTypes: ProcessType[] = [];
         processTypes = await Promise.all(processType_ids.map(async (processType_id) => {
             return await ProcessTemplatesHelper.getProcessTypeById(processType_id);
         }))
-
         sector.processTypes = processTypes;
+
         return sector;
     }
 
     private async updateOfficials(sector: Sector, usersIds: number[], action: "committer" | "responsible"): Promise<Sector> {
         if (!usersIds) return sector;
-        // CODE FOR SMART ADDING
-        // const currentResponsibleUsers: User[] = await sector.responsibleUsers;
-        // let existingResponsibleUserIds: number[] = (currentResponsibleUsers).map((responsibleUser) => responsibleUser.id);
-        // let users: User[] = currentResponsibleUsers;
-        // usersIds.filter(id => !(existingResponsibleUserIds.includes(id))).forEach(async (id) => {
-        //     const usr: User = await UsersHelper.getUserById(id);
-        //     users.push(usr);
-        // })
-        // sector.responsibleUsers = Promise.resolve(users);
 
         let users: User[] = [];
         users = await Promise.all(usersIds.map(async (id) => {
             return await UsersHelper.getUserById(id);
         }))
+
         switch (action) {
             case 'committer':
                 sector.committingUsers = users;
@@ -149,16 +143,19 @@ export class SectorsService {
                 sector.responsibleUsers = users;
                 break;
         }
+
         return sector;
     }
 
     private async updateRelatedProcesses(sector: Sector, processTemplatesIds: string[]): Promise<Sector> {
         if (!processTemplatesIds) return sector;
+
         let relatedProcesses: ProcessTemplate[] = [];
         relatedProcesses = await Promise.all(processTemplatesIds.map(async (id) => {
             return await ProcessTemplatesHelper.getProcessTemplateById(id)
         }))
         sector.relatedProcesses = relatedProcesses;
+
         return sector;
     }
 
