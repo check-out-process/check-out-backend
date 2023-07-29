934291a0bf94b8ef41e1e7e439d3fd846df07390
afekl <afekev55@gmail.com>
2023-07-29 11:04:31 +0300

    add tests to Auth Service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -6,69 +6,159 @@ import { createMock } from '@golevelup/ts-jest';
 import { LogInParams, UserCreationParams } from '@checkout/types';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+class MockUsersService {
+    getUserByPhoneNumber = jest.fn();
+}
+
 describe('Auth Service', () => {
     let service: AuthService;
+    let usersService: MockUsersService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [AuthService,
-                { provide: UsersService, useValue: createMock<UsersService>() },
+                { provide: UsersService, useValue: MockUsersService },
                 { provide: TokensService, useValue: createMock<TokensService>() },
 
             ],
         }).compile();
         service = module.get<AuthService>(AuthService);
+        usersService = module.get<MockUsersService>(UsersService);
+
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+
     describe('login', () => {
-        it('null phoneNumber should throw error', async () => {
-            const logInParams: LogInParams = {
-                phoneNumber: null,
+        let logInParams: LogInParams;
+
+        beforeEach(() => {
+            logInParams = {
+                phoneNumber: 'string',
                 password: 'string',
                 tokens: []
             }
+        })
+
+        it('should throw error when phoneNumber is null', async () => {
+            // Arrange
+            logInParams.phoneNumber = null;
+
+            // Act + Assert
             await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
         });
 
-        it('null password should throw error', async () => {
-            const logInParams: LogInParams = {
-                phoneNumber: 'string',
-                password: null,
-                tokens: []
-            }
+        it('should throw error when password is null', async () => {
+            // Arrange
+            logInParams.password = null;
+
+
+            // Act + Assert
+            await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        });
+
+        it('should throw error when getUserByPhoneNumber return null', async () => {
+            // Arrange
+            usersService.getUserByPhoneNumber = jest.fn(null);
+
+            // Act + Assert
+            await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
+        });
+
+        it('should throw error when logInParams password not match user password', async () => {
+            // Arrange
+            const expectedUser = {
+                id: 1,
+                fullname: 'afek lev',
+                username: 'afek lev',
+                phoneNumber: '0527364455',
+                password: '',
+                job: {
+                    id: '',
+                    name: ''
+                },
+                role: {
+                    id: '',
+                    name: ''
+                },
+                sectors: undefined,
+                sectors_in_responsibility: undefined
+            };
+            usersService.getUserByPhoneNumber.mockResolvedValue(expectedUser);
+
+            // Act + Assert
             await expect(service.login(logInParams)).rejects.toEqual(new HttpException("phoneNumber or password not correct", HttpStatus.NOT_FOUND));
         });
     })
 
     describe('register', () => {
-        it('null phoneNumber should throw error', async () => {
-            const logInParams: UserCreationParams = {
+        let userCreationParams: UserCreationParams;
+
+        beforeEach(() => {
+            userCreationParams = {
                 id: 1,
                 fullname: 'string',
                 username: 'string',
                 password: 'string',
                 jobId: 'string',
                 roleId: 'string',
-                phoneNumber: null
-            }
-            await expect(service.register(logInParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
-        });
-
-        it('null password should throw error', async () => {
-            const logInParams: UserCreationParams = {
-                id: 1,
-                fullname: 'string',
-                username: 'string',
-                password: null,
-                jobId: 'string',
-                roleId: 'string',
                 phoneNumber: 'string'
             }
-            await expect(service.register(logInParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
+        })
+
+        it('should throw error when phoneNumber is null', async () => {
+            // Arrange
+            userCreationParams.phoneNumber = null;
+
+            // Act + Assert
+            await expect(service.register(userCreationParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
+        });
+
+        it('should throw error when password is null', async () => {
+            // Arrange
+            userCreationParams.password = null;
+
+            // Act + Assert
+            await expect(service.register(userCreationParams)).rejects.toEqual(new HttpException("phoneNumber and password should not be null", HttpStatus.BAD_REQUEST));
+        });
+
+        it('should throw error when getUserByPhoneNumber not return null', async () => {
+            // Arrange
+            const expectedUser = {
+                id: 1,
+                fullname: 'afek lev',
+                username: 'afek lev',
+                phoneNumber: '0527364455',
+                password: '',
+                job: {
+                    id: '',
+                    name: ''
+                },
+                role: {
+                    id: '',
+                    name: ''
+                },
+                sectors: undefined,
+                sectors_in_responsibility: undefined
+            };
+            usersService.getUserByPhoneNumber.mockResolvedValue(expectedUser);
+
+            // Act + Assert
+            await expect(service.register(userCreationParams)).rejects.toEqual(new HttpException("user already registrated", HttpStatus.CONFLICT));
         });
     })
 
+    describe('logout', () => {
+        it('should throw error when token is null', async () => {
+            await expect(service.logout(null)).rejects.toEqual(new HttpException("Invalid refresh token", HttpStatus.UNAUTHORIZED));
+        });
+    })
+
+    describe('refreshTokens', () => {
+        it('should throw error when token is null', async () => {
+            await expect(service.logout(null)).rejects.toEqual(new HttpException("Invalid refresh token", HttpStatus.UNAUTHORIZED));
+        });
+    })
 });
