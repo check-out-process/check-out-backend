573db522a26729af95f6634b2129a542224da636
lior bahar <lior.bahar1412@gmail.com>
2023-08-01 16:01:52 +0300

    fix broken ut and ut

--- src/jobs/jobs.service.spec.ts
+++ src/jobs/jobs.service.spec.ts
@@ -1,18 +1,93 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobsService } from './jobs.service';
+import { Job } from './jobs.entities';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+function randomUUID() {
+  return 'test-' + Math.random().toString(36).substr(2, 9);
+}
 
 describe('JobsService', () => {
-  let service: JobsService;
+  let jobsService: JobsService;
+  let jobsRepository: Repository<Job>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JobsService],
+      providers: [
+        JobsService,
+        {
+          provide: 'JOB_REPOSITORY',
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<JobsService>(JobsService);
+    jobsService = module.get<JobsService>(JobsService);
+    jobsRepository = module.get<Repository<Job>>('JOB_REPOSITORY');
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createJob', () => {
+    it('should create a job and return it', async () => {
+      // Arrange
+      const createJobParams = { /* provide necessary data for creating a job */ };
+
+      // Mock the repository's create and save methods
+      const createdJob = new Job();
+      jest.spyOn(jobsRepository, 'create').mockReturnValue(createdJob);
+      jest.spyOn(jobsRepository, 'save').mockResolvedValue(createdJob);
+
+      // Act
+      const result = await jobsService.createJob(createJobParams as any);
+
+      // Assert
+      expect(result).toBe(createdJob);
+      expect(jobsRepository.create).toHaveBeenCalledTimes(1);
+      expect(jobsRepository.save).toHaveBeenCalledWith(createdJob);
+    });
+  });
+
+  describe('getJobs', () => {
+    it('should return an array of jobs', async () => {
+      // Arrange
+      const jobs = [new Job(), new Job()];
+      jest.spyOn(jobsRepository, 'find').mockResolvedValue(jobs);
+
+      // Act
+      const result = await jobsService.getJobs();
+
+      // Assert
+      expect(result).toEqual(jobs);
+      expect(jobsRepository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getJob', () => {
+    it('should return a single job by its ID', async () => {
+      // Arrange
+      const jobId = randomUUID();
+      const job = new Job();
+      jest.spyOn(jobsRepository, 'findOne').mockResolvedValue(job);
+
+      // Act
+      const result = await jobsService.getJob(jobId);
+
+      // Assert
+      expect(result).toBe(job);
+      expect(jobsRepository.findOne).toHaveBeenCalledWith({ where: { id: jobId } });
+    });
+
+    it('should return undefined if the job does not exist', async () => {
+      // Arrange
+      const jobId = randomUUID();
+      jest.spyOn(jobsRepository, 'findOne').mockResolvedValue(undefined);
+
+      // Act
+      const result = await jobsService.getJob(jobId);
+
+      // Assert
+      expect(result).toBeUndefined();
+      expect(jobsRepository.findOne).toHaveBeenCalledWith({ where: { id: jobId } });
+    });
   });
 });
